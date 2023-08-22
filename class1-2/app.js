9fd9ac47b7b5802634250658e534ab2bb6e28405
NAVEEN ASGHAR <122675368+naveenasghar@users.noreply.github.com>
2023-08-22 20:13:24 +0500

    Add files via upload

---
+++ class1-2/app.js
@@ -0,0 +1,30 @@
+// alert("hello world")
+// var greeting=("hello world");
+// var num=78.98;
+// alert(num);
+// var bool=true;
+// alert(bool);
+// var num=5;
+// alert(num++);
+// alert(num);
+// var num=6;
+// ++num; 7
+// var num2=num--
+
+// alert(++num + num2);
+// var num=2;
+// ++num;
+// var num2=num--;
+// alert(++num + num2);
+// var num=8;
+// var text="hello";
+// var text2=7;
+// console.log((text+text2)+num);
+// var name=prompt("what is your name");
+// console.log("hello " +name);
+// var num=+prompt("what is your fav num" ,7);
+// console.log(num);
+// var num=8;
+// var num2=8;
+// console.log(num != num2);
+var num
