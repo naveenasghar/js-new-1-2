03ceee1e1d8ae93de5b74d2c60f84b354d33f830
NAVEEN ASGHAR <122675368+naveenasghar@users.noreply.github.com>
2023-08-26 20:35:03 +0500

    Add files via upload
    class 3

--- class3/app.js
+++ class3/app.js
@@ -41,20 +41,63 @@
 // }else{
 //     console.log("error")
 // }
-var num=+prompt("enter a percentage");
-var grade;
-if(num >=0 && num<=100){
-if(num >= 80){
-    grade="a+"
-}else if(num >=70){
-    grade="a"
-}else if(num >= 60){
-    grade="b"
-}else if(num <=50){
-    grade="fail"
-}
-console.log(grade)
-}
-else{
-console.log("error")
-}
+// var num=+prompt("enter a percentage");
+// var grade;
+// if(num >=0 && num<=100){
+// if(num >= 80){
+//     grade="a+"
+// }else if(num >=70){
+//     grade="a"
+// }else if(num >= 60){
+//     grade="b"
+// }else if(num <=50){
+//     grade="fail"
+// }
+// console.log(grade)
+// }
+// else{
+// console.log("error")
+// }
+// var num=+prompt("enter a percentage");
+// var grade;
+// if(num >=0 && num <=100){
+//     if(num>=80){
+//         grade="a+";
+//     }else if(num>=70){
+//         grade="a"
+//     }else if(num>=60){
+//         grade="b";
+//     }else if(num>=50){
+//         grade="c";
+//     }else if(num<=50){
+//         grade="fail"
+//     }
+//     document.write(grade);
+// }else{
+// document.write("error")};
+// var months=["january","february","march"];
+// months[6]="april";
+// console.log(months);
+// var days=["mon","tues","wed","thurs"];
+// days.push("friday");
+// console.log(days);
+// var days=["mon","tues","wed","thurs"];
+// days.push("friday");
+// days.pop();
+// console.og(days);
+// days.shift();
+// days.splice(0,1 ,"march","april");
+// console.log(days);
+// var months=["jan","feb","march","april"];
+// months.slice(0,3);
+// console.log(months);
+// console.log(months.slice(0,4));
+var months=["sunday","monday","tues","wed"];
+// months.push("friday");
+// months.pop(4);
+// months.unshift("saturday");
+// months.shift("saturday");
+// months.splice(1,3);
+console.log(months);
+console.log(months.slice(0,2));
+
